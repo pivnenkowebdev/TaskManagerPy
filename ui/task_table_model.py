140fa1f71d9dd33607c092ec5030bf72b8582a27
pivnenkowebdev <pivnenkowevdev@gmail.com>
2026-04-03 13:02:15 +0300

    feat: add cash and update this

--- ui/task_table_model.py
+++ ui/task_table_model.py
@@ -6,11 +6,18 @@ class TaskTableModel(QAbstractTableModel):
     def __init__(self, service):
         super().__init__()
         self._service = service
-        self._headers = ["ID", "Name", "Priority", "Date"]
+        self._headers = ["Name", "Priority", "Date"]
+        self.tasks = self._service.get_all()
+
+    # обновление кеша
+    def refresh(self):
+        self.beginResetModel()
+        self.tasks = self._service.get_all()
+        self.endResetModel()
 
     # количество строк
     def rowCount(self, parent=QModelIndex()):
-        return len(self._service.get_all())
+        return len(self.tasks)
 
     # количество колонок
     def columnCount(self, parent=QModelIndex()):
@@ -22,18 +29,15 @@ class TaskTableModel(QAbstractTableModel):
             return None
 
         if role == Qt.DisplayRole:
-            tasks = self._service.get_all()
-            task = tasks[index.row()]
+            task = self.tasks[index.row()]
 
             column = index.column()
 
             if column == 0:
-                return task.id
-            elif column == 1:
                 return task.name
-            elif column == 2:
+            elif column == 1:
                 return task.priority
-            elif column == 3:
+            elif column == 2:
                 return task.date
 
         return None

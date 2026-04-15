1566dca7231ac735dc5c7fe5cfefdb7a699b83bf
pivnenkowebdev <pivnenkowevdev@gmail.com>
2026-04-15 17:31:23 +0300

    feat: add remove task

--- ui/main_window.py
+++ ui/main_window.py
@@ -1,9 +1,11 @@
 from PySide6.QtWidgets import (
+    QAbstractItemView,
     QMainWindow,
     QWidget,
     QVBoxLayout,
     QPushButton,
     QTableView,
+    QMessageBox
 )
 
 from storage.json_storage import JsonStorage
@@ -29,20 +31,29 @@ class MainWindow(QMainWindow):
         self.table = QTableView()
         self.table.setModel(self.model)
 
+        # удаление
+        self.table.setSelectionBehavior(QAbstractItemView.SelectRows)
+        self.table.setSelectionMode(QAbstractItemView.SingleSelection)
+
         self.add_button = QPushButton("Добавить задачу")
         self.add_button.clicked.connect(self.add_task)
+
+        # удаление
+        self.delete_button = QPushButton("Удалить задачу")
+        self.delete_button.clicked.connect(self.delete_task)
 
         # layout
         layout = QVBoxLayout()
         layout.addWidget(self.table)
         layout.addWidget(self.add_button)
+        layout.addWidget(self.delete_button)
 
         container = QWidget()
         container.setLayout(layout)
 
         self.setCentralWidget(container)
 
-    # # --- логика ---
+    # --- логика ---
     # def add_task(self):
     #     self.service.add_task("Test Task", "High", "2026-01-01")
     #     self.model.refresh()
@@ -54,4 +65,27 @@ class MainWindow(QMainWindow):
             name, priority, date = dialog.get_data()
 
             self.service.add_task(name, priority, date)
+            self.model.refresh()
+
+    def delete_task(self):
+        selection = self.table.selectionModel().selectedRows()
+
+        if not selection:
+            QMessageBox.warning(self, "Ошибка", "Выберите задачу")
+            return
+
+        row = selection[0].row()
+
+        # получаем задачу из модели
+        task = self.model.tasks[row]
+
+        reply = QMessageBox.question(
+            self,
+            "Подтверждение",
+            f"Удалить задачу '{task.name}'?",
+            QMessageBox.Yes | QMessageBox.No,
+        )
+
+        if reply == QMessageBox.Yes:
+            self.service.delete_task(task.id)
             self.model.refresh()
